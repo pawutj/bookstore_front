de6c2f99067034ec0ca444309ac5a3b4815872cf
22p26c0377 <pawut.j@gmail.com>
2021-08-24 05:32:04 +0800

    Add colors to styles

---
+++ src/styles/ci.js
@@ -0,0 +1,9 @@
+export const colors = {
+  mainBg1: "#00af70",
+  mainBg2: "#37c13d",
+  mainColor: "#00bf6c",
+  fontMenu: "#5a5a5a",
+  font: "#1d6548",
+  fontGray: "#5a5a5a",
+  link: "#00bf6c",
+};
